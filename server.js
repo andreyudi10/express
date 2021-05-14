d846d4c9ec6375db8d13a1898af9a4dfb7a538cb
andreyudi10 <andrianus.yudi@gmail.com>
2021-05-15 04:30:45 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('express')
+
+const app = express()
+
+app.get('/api', cors(), (req,res) => {
+    const dataApi = [
+        "contohnya string ini"
+    ]
+
+    res.json(dataApi)
+})
+
+app.get('/api/lainnya', cors(), (req,res) => {
+    const dataApi = [
+        {nama:"andri",rumah:"yogyakarta"},
+        {nama:"yudi",rumah:"semarang"},
+        {nama:"pratomo",rumah:"surakarta"},
+    ]
+
+    res.json(dataApi)
+})
+
+const port = 5000;
+app.listen( port, () => {
+    console.log(`runnin in ${port}`)
+} )
